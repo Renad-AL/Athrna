e9fae2b138ee31e844b26b2efb0851ce1c8f496e
amalalm <amalmoh.mtr@gmail.com>
2025-01-01 20:06:10 +0300

    a

--- Athrna/ContentView.swift
+++ Athrna/ContentView.swift
@@ -1,8 +1,10 @@
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     @AppStorage("selectedCharacter") private var selectedCharacter: String = "None"
     @State private var navigateToNextView = false
+    @State private var audioPlayer: AVAudioPlayer?
 
     var body: some View {
         NavigationStack {
@@ -42,6 +44,22 @@ struct ContentView: View {
                                 .frame(width: 150, height: 60)
                                 .overlay(Text("سارة").font(.title).foregroundColor(.white))
                         }
+
+                        VStack(spacing: 10) {
+                            Button(action: {
+                                selectedCharacter = "Boy"
+                                navigateToNextView = true
+                            }) {
+                                Image("Boyy")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 350, height: 450)
+                            }
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.purple.opacity(0.8))
+                                .frame(width: 150, height: 60)
+                                .overlay(Text("سعد").font(.title).foregroundColor(.white))
+                        }
                     }
                     .padding(.bottom, 100)
 
@@ -54,6 +72,9 @@ struct ContentView: View {
                 }
             }
             .navigationBarHidden(true)
+            .onAppear {
+                playSound("HelloHeroes")
+            }
         }
     }
 
@@ -61,9 +82,26 @@ struct ContentView: View {
     private func nextView() -> some View {
         if selectedCharacter == "Girl" {
             AtharListViewgirl(viewModel: ViewModelgirl())
+        } else if selectedCharacter == "Boy" {
+            AtharListViewBoy(viewModel: ViewModelboy())
         } else {
             Text("لم يتم اختيار شخصية")
         }
     }
+
+    private func playSound(_ sound: String) {
+        guard let url = Bundle.main.url(forResource: sound, withExtension: "mp3") else { return }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
 }
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
